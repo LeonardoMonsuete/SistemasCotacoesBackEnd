c11fceecd8ceec844b574e1064fa70cd8880a7cf
KleitonBarone <kleitonpolar6@gmail.com>
2021-06-05 17:18:51 -0300

    ajustando banco para deploy

--- models/db.js
+++ models/db.js
@@ -1,8 +1,21 @@
 import Sequelize from 'sequelize'
-const sequelize = new Sequelize('sistema_cotacao', 'root', 'admin', {
-    host: 'localhost',
-    dialect: 'mysql'
-})
+
+if(process.env.DATABASE_URL) {
+    var sequelize = new Sequelize(process.env.DATABASE_URL,
+        {
+          dialect: "postgres",
+          protocol: "postgres",
+          port: 5432,
+          host: "localhost",
+          logging: false //false
+       });
+} else {
+    var sequelize = new Sequelize('sistema_cotacao', 'root', 'admin', {
+        host: 'localhost',
+        dialect: 'mysql'
+    })
+}
+
 
 sequelize.sync()
 
